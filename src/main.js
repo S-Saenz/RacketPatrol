0cb3b7b558e35c3d2e61ecc1c35a0aa5eb89e104
Saenz <sarah.r.saenz@gmail.com>
2020-04-13 19:22:06 -0700

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,13 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [ Menu, Play ]
+}
+let game = new Phaser.Game(config);
+// define game settings
+game.settings = {
+    spaceshipSpeed: 3,
+    gameTimer: 60000    
+}
+let keyF, keyLEFT, keyRIGHT;
